44206cf9b5d2ad2d71545726fc3cb90b311d776c
Sahil Agarwal <63807736+sahilagarwal1538@users.noreply.github.com>
2026-01-06 15:53:44 +0530

    Adding client side main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
-console.log('Loaded!');
+console.log('Hi! We are printing on the console');
+
+// change the main content
+var element = document.getElementById('main content');
+element.innerHTML = 'New content here!';
+
+// move the image
+var img = document.getElementById('madi');
+img.onclick = function () {
+img.style.marginLeft = '100px';
+};
